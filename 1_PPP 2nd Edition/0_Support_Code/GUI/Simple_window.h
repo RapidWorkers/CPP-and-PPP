3f571937b29568ea29c44b4098a919945ffa6588
SOOHYUN KIM <RapidWorkers@users.noreply.github.com>
2019-05-16 11:08:55 +0900

    fix for linux 2

--- 1_PPP 2nd Edition/0_Support_Code/GUI/Simple_window.h
+++ 1_PPP 2nd Edition/0_Support_Code/GUI/Simple_window.h
@@ -8,7 +8,7 @@
 
 struct Simple_window : Graph_lib::Window {
 	Simple_window(Point xy, int w, int h, const string& title )
-	: Graph_lib::Window(xy,w,h,title),
+	: Window(xy,w,h,title),
 	  button_pushed(false),
 	  next_button(Point(x_max()-70,0), 70, 20, "Next", cb_next) { attach(next_button); }
 	
