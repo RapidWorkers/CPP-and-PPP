177e915829e1473555a994b90b1b64a1c19d22c3
RapidWorkers <RapidWorkers@users.noreply.github.com>
2019-05-16 11:01:52 +0900

    fix for linux

--- 1_PPP 2nd Edition/0_Support_Code/GUI/Simple_window.h
+++ 1_PPP 2nd Edition/0_Support_Code/GUI/Simple_window.h
@@ -1,14 +1,14 @@
 
 #include "GUI.h"	// for Simple_window only (doesn't really belong in Window.h)
 
-using namespace Graph_lib;
+//using namespace Graph_lib;
 
 // Simple_window is basic scaffolding for ultra-simple interaction with graphics
 // it provides one window with one "next" button for ultra-simple animation
 
-struct Simple_window : Window {
+struct Simple_window : Graph_lib::Window {
 	Simple_window(Point xy, int w, int h, const string& title )
-	: Window(xy,w,h,title),
+	: Graph_lib::Window(xy,w,h,title),
 	  button_pushed(false),
 	  next_button(Point(x_max()-70,0), 70, 20, "Next", cb_next) { attach(next_button); }
 	
@@ -22,11 +22,11 @@ struct Simple_window : Window {
 		Fl::redraw();
 	}
 
-	Button next_button;
+	Graph_lib::Button next_button;
 private:
 	bool button_pushed;
 	
-	static void cb_next(Address, Address addr) // callback for next_button
+	static void cb_next(Graph_lib::Address, Graph_lib::Address addr) // callback for next_button
 	//	{ reference_to<Simple_window>(addr).next(); }
 	{
 		static_cast<Simple_window*>(addr)->next();
