3132ea2684653991711d4985016267cccd9b9aa2
RapidWorkers <RapidWorkers@users.noreply.github.com>
2019-10-17 09:56:27 +0900

    Update LZ77.h

--- 2_Personal/Deflate/Deflate/LZ77.h
+++ 2_Personal/Deflate/Deflate/LZ77.h
@@ -1,4 +1,19 @@
 #ifndef DEFLATE_LZ77_H
 #define DEFLATE_LZ77_H
 
+#include <vector>
+#include <memory>
+#include <fstream>
+#include <exception>
+
+class LZ77{
+    static const int BLOCKSZ = 65536;
+    using BYTE = unsigned char;
+private:
+  BYTE inStream[BLOCKSZ];
+
+
+public:
+};
+
 #endif //DEFLATE_LZ77_H
